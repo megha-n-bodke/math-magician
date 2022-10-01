7f690a05e3deb965a720f82cce81941cb20dac21
Megha Bodke <megha.n.bodke@gmail.com>
2022-10-01 16:52:36 +0530

    Fix linter errors

--- src/__test__/operate.test.js
+++ src/__test__/operate.test.js
@@ -1,16 +1,16 @@
-import operate from "../logic/operate";
+import operate from '../logic/operate';
 
-test("Addition", () => {
-  expect(operate(1, 2, "+")).toBe("3");
+test('Addition', () => {
+  expect(operate(1, 2, '+')).toBe('3');
 });
-test("Substraction", () => {
-  expect(operate(3, 2, "-")).toBe("1");
-});
-
-test("Multiplication", () => {
-  expect(operate(3, 2, "x")).toBe("6");
+test('Substraction', () => {
+  expect(operate(3, 2, '-')).toBe('1');
 });
 
-test("Multiplication", () => {
-  expect(operate(4, 2, "÷")).toBe("2");
+test('Multiplication', () => {
+  expect(operate(3, 2, 'x')).toBe('6');
+});
+
+test('Multiplication', () => {
+  expect(operate(4, 2, '÷')).toBe('2');
 });
